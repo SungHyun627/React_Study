3d7e313d8f9c214dfb89f55f0a028f5b6eed146d
SungHyun627 <kksscc4@gmail.com>
2021-11-18 14:46:15 +0900

    Add Wrapper component

--- react-intro/src/App.js
+++ react-intro/src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
 import Hello from "./Hello";
+import Wrapper from "./Wrapper";
 
 function App() {
   const name = "react";
 
   const style = {
-    backgroundColor: "black",
+    backgroundColor: "red",
     color: "aqua",
     fontSize: 24,
     padding: "1rem",
   };
 
   return (
-    <>
-      <Hello />
+    <Wrapper>
+      <Hello name={name} color="red" />
+      <Hello color="pink" />
       <div style={style}>{name}</div>
       <div className="gray-box"></div>
-    </>
+    </Wrapper>
   );
 }
 
