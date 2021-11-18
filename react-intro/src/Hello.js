e871c9f0de7dd8708d143ed39ad716d163d44096
SungHyun627 <kksscc4@gmail.com>
2021-11-18 14:46:53 +0900

    destruct allocation

--- react-intro/src/Hello.js
+++ react-intro/src/Hello.js
@@ -1,5 +1,10 @@
-const Hello = () => {
-  return <div>안녕하세요</div>;
+const Hello = ({ color, name }) => {
+  // 비구조화 할당
+  return <div style={{ color }}>안녕하세요 {name}</div>;
 };
 
+// 기본값 설정
+Hello.defaultProps = {
+  name: "이름없음",
+};
 export default Hello;
