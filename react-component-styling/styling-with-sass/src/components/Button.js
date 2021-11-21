af6b25e09829dbfa270205c9b194e898a9cd622c
SungHyun627 <kksscc4@gmail.com>
2021-11-21 14:22:42 +0900

    set the different button's color and margin

--- react-component-styling/styling-with-sass/src/components/Button.js
+++ react-component-styling/styling-with-sass/src/components/Button.js
@@ -1,13 +1,16 @@
 import "./Button.scss";
-import classNames from 'classnames';
+import classNames from "classnames";
 
-const Button = ({ children, size }) => {
-  return <button className={classNames('Button', size)}>{children}</button>;
+const Button = ({ children, size, color }) => {
+  return (
+    <button className={classNames("Button", size, color)}>{children}</button>
+  );
 };
 
 // size defaultprops 이용
 Button.defaultProps = {
   size: "medium",
+  color: "blue",
 };
 
 export default Button;
