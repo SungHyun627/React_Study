cddc298e032740011b2a6b8f89fc025d7d5f50f2
SungHyun627 <kksscc4@gmail.com>
2021-11-21 14:43:35 +0900

    Set the button's width in proportion to parent's width

--- react-component-styling/styling-with-sass/src/components/Button.js
+++ react-component-styling/styling-with-sass/src/components/Button.js
@@ -1,10 +1,12 @@
 import "./Button.scss";
 import classNames from "classnames";
 
-const Button = ({ children, size, color, outline }) => {
+const Button = ({ children, size, color, outline, fullWidth }) => {
   return (
     // outline이 true일 때에만 button에 outline CSS클래스 적용
-    <button className={classNames("Button", size, color, { outline })}>
+    <button
+      className={classNames("Button", size, color, { outline, fullWidth })}
+    >
       {children}
     </button>
   );
