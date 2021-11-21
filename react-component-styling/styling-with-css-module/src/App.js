5a618041b918215528d46d27a328295a86335976
SungHyun627 <kksscc4@gmail.com>
2021-11-21 19:37:40 +0900

    Add CheckBox component

--- react-component-styling/styling-with-css-module/src/App.js
+++ react-component-styling/styling-with-css-module/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 import Box from "./components/Box";
+import CheckBox from "./components/CheckBox";
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = (e) => {
+    setCheck(e.target.checked);
+  };
   return (
     <div className="App">
       <Box />
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check:</b>
+        {check ? "true" : "false"}
+      </p>
     </div>
   );
 }
