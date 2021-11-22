e50eb8fe7511414815536d2d4633f3cd9b60e9ca
SungHyun627 <kksscc4@gmail.com>
2021-11-22 15:45:18 +0900

    use ThemeProvivder's palette using props

--- react-component-styling/styling-with-styled-components/src/components/Button.js
+++ react-component-styling/styling-with-styled-components/src/components/Button.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { darken, lighten } from "polished";
 
 const StyledButton = styled.button`
@@ -15,21 +15,30 @@ const StyledButton = styled.button`
 
   height: 2.25rem;
   font-size: 1rem;
-
-  background: #228be6;
-  &:hover {
-    background: ${lighten(0.1, "#228be6")};
-  }
-  &:active {
-    background: ${darken(0.1, "#228be6")};
-  }
+  ${(props) => {
+    const selected = props.theme.palette[props.color];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+    `;
+  }}
 
   & + & {
-    margin-left: 1rem;}
+    margin-left: 1rem;
   }
 `;
 const Button = ({ children, ...rest }) => {
   return <StyledButton {...rest}>{children}</StyledButton>;
+};
+
+// 버튼의 초기 색깔 blue로 설정
+Button.defaultProps = {
+  color: "blue",
 };
 
 export default Button;
