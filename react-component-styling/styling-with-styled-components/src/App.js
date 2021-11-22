ee6892270c4c0b7ca7af72d5555c9b2323118eb8
SungHyun627 <kksscc4@gmail.com>
2021-11-22 21:42:18 +0900

    make the visibility function using usestate

--- react-component-styling/styling-with-styled-components/src/App.js
+++ react-component-styling/styling-with-styled-components/src/App.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import styled, { css, ThemeProvider } from "styled-components";
 import Button from "./components/Button";
+import Dialog from "./components/Dialog";
 
 const Circle = styled.div`
   width: 2rem;
@@ -31,6 +33,22 @@ const ButtonGroup = styled.div`
 `;
 
 const App = () => {
+  const [dialog, setDialog] = useState(false);
+
+  const onClick = () => {
+    setDialog(true);
+  };
+
+  const onConfirm = () => {
+    console.log("확인");
+    setDialog(true);
+  };
+
+  const onCancel = () => {
+    console.log("취소");
+    setDialog(false);
+  };
+
   return (
     // ThemeProvider 내부 렌더링된 styled-components로 만든 컴포넌트는 palette를 조회하여 사용 가능
     <ThemeProvider
@@ -42,54 +60,66 @@ const App = () => {
         },
       }}
     >
-      <AppBlock>
-        <Circle color="blue" />
-        <ButtonGroup>
-          <Button size="large">BUTTON</Button>
-          <Button>BUTTON</Button>
-          <Button size="small">BUTTON</Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="gray" size="large">
-            BUTTON
-          </Button>
-          <Button color="gray">BUTTON</Button>
-          <Button color="gray" size="small">
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button color="pink" size="large">
-            BUTTON
-          </Button>
-          <Button color="pink">BUTTON</Button>
-          <Button color="pink" size="small">
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size="large" outline>
-            BUTTON
-          </Button>
-          <Button color="gray" outline>
-            BUTTON
-          </Button>
-          <Button color="pink" size="small" outline>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-        <ButtonGroup>
-          <Button size="large" fullWidth>
-            BUTTON
-          </Button>
-          <Button size="large" color="gray" fullWidth>
-            BUTTON
-          </Button>
-          <Button size="large" color="pink" fullWidth>
-            BUTTON
-          </Button>
-        </ButtonGroup>
-      </AppBlock>
+      <>
+        <AppBlock>
+          <Circle color="blue" />
+          <ButtonGroup>
+            <Button size="large">BUTTON</Button>
+            <Button>BUTTON</Button>
+            <Button size="small">BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="gray" size="large">
+              BUTTON
+            </Button>
+            <Button color="gray">BUTTON</Button>
+            <Button color="gray" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="pink" size="large">
+              BUTTON
+            </Button>
+            <Button color="pink">BUTTON</Button>
+            <Button color="pink" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" outline>
+              BUTTON
+            </Button>
+            <Button color="gray" outline>
+              BUTTON
+            </Button>
+            <Button color="pink" size="small" outline>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" fullWidth>
+              BUTTON
+            </Button>
+            <Button size="large" color="gray" fullWidth>
+              BUTTON
+            </Button>
+            <Button size="large" color="pink" fullWidth onClick={onClick}>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+      </>
     </ThemeProvider>
   );
 };
