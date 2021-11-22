785dfb08d4f659328ba25002a4de12419ffe5797
SungHyun627 <kksscc4@gmail.com>
2021-11-22 12:19:47 +0900

    add button component in App component

--- react-component-styling/styling-with-styled-components/src/App.js
+++ react-component-styling/styling-with-styled-components/src/App.js
@@ -1,13 +1,35 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import Button from "./components/Button";
 
 const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: black;
+  width: 2rem;
+  height: 2rem;
+  background: ${(props) => props.color || "black"};
   border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    // css를 조건부로 보여주고 싶다면 css를 사용
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
 `;
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
 const App = () => {
-  return <Circle />;
+  return (
+    <AppBlock>
+      <Circle color="blue" />
+      <Button>BUTTON</Button>
+    </AppBlock>
+  );
 };
 
 export default App;
