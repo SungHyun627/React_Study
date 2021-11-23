3f32bb3f6882653d3944e6f25ff2a05f532a2242
SungHyun627 <kksscc4@gmail.com>
2021-11-23 15:45:11 +0900

    complete Todoitem component

--- react-todolist/src/components/TodoItem.js
+++ react-todolist/src/components/TodoItem.js
@@ -1,5 +1,7 @@
+import { memo } from "react";
 import styled, { css } from "styled-components";
 import { MdDone, MdDelete } from "react-icons/md";
+import { useTodoDispatch } from "./TodoContext";
 
 const Remove = styled.div`
   display: flex;
@@ -56,15 +58,21 @@ const Text = styled.div`
     `}
 `;
 const TodoItem = ({ id, done, text }) => {
+  const dispatch = useTodoDispatch();
+  const onToggle = () => dispatch({ type: "TOGGLE", id });
+  const onRemove = () => dispatch({ type: "REMOVE", id });
   return (
     <TodoItmeBlock>
-      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <CheckCircle done={done} onClick={onToggle}>
+        {done && <MdDone />}
+      </CheckCircle>
       <Text done={done}>{text}</Text>
-      <Remove>
+      <Remove onClick={onRemove}>
         <MdDelete />
       </Remove>
     </TodoItmeBlock>
   );
 };
 
-export default TodoItem;
+// 불필요한 rerendering 방지
+export default memo(TodoItem);
