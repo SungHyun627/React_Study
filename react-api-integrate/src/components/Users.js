56018733a1b6f4144379773f7945adfc738f5671
SungHyun627 <kksscc4@gmail.com>
2021-11-24 13:40:22 +0900

    버튼을 사용하여 API 재요청

--- react-api-integrate/src/components/Users.js
+++ react-api-integrate/src/components/Users.js
@@ -6,28 +6,26 @@ const Users = () => {
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const fetchUsers = async () => {
+    try {
+      //   요청 시작 시 users,error 초기화, lading => true
+      setUsers(null);
+      setLoading(true);
+      setError(null);
+
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      // 데이터 받고
+      setUsers(response.data);
+    } catch (e) {
+      // Error 발생
+      setError(e);
+    }
+    setLoading(false);
+  };
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        //   요청 시작 시 users,error 초기화, lading => true
-        setUsers(null);
-        setLoading(true);
-        setError(null);
-
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        // 데이터 받고
-        setUsers(response.data);
-      } catch (e) {
-        // Error 발생
-        setError(e);
-      }
-      setLoading(false);
-    };
-
-    // fetchUser함수 실행
     fetchUsers();
   }, []);
 
@@ -36,14 +34,18 @@ const Users = () => {
   if (!users) return null;
 
   return (
-    // User 배열 랜더링
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      {/*User 배열 랜더링 */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      {/* 버튼을 사용하여 다시 불러오기 */}
+      <button onClick={fetchUsers}>다시 불러오기</button>
+    </>
   );
 };
 
