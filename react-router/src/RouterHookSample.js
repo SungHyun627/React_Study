be29e4228e7f22dabe1ba40bf125c9e5d8eaf0eb
SungHyun627 <kksscc4@gmail.com>
2021-11-25 15:14:08 +0900

    reactrouter hooks 이용하여 history, location, match 조회

---
+++ react-router/src/RouterHookSample.js
@@ -0,0 +1,11 @@
+import { useHistory, useLocation, useRouteMatch } from "react-router-dom";
+
+const RouterHookSample = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const match = useRouteMatch();
+  console.log({ history, location, match });
+  return null;
+};
+
+export default RouterHookSample;
