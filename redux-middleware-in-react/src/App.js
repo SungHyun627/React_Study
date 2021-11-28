0589306ae64b53d9414f0fdec7893da42c4ebb42
SungHyun627 <kksscc4@gmail.com>
2021-11-28 16:18:30 +0900

    App에서 라우트 설정

--- redux-middleware-in-react/src/App.js
+++ redux-middleware-in-react/src/App.js
@@ -1,13 +1,17 @@
-import "./App.css";
-import CounterContainer from "./containers/CounterContainer";
-import PostListContainer from "./containers/PostListContainer";
+import { Route } from "react-router-dom";
+// import CounterContainer from "./containers/CounterContainer";
+// import PostListContainer from "./containers/PostListContainer";
+import PostListPage from "./pages/PostListPage";
+import PostPage from "./pages/PostPage";
 
 function App() {
   return (
-    <div>
-      <CounterContainer />
-      <PostListContainer />
-    </div>
+    <>
+      {/* <CounterContainer />
+      <PostListContainer /> */}
+      <Route path="/" component={PostListPage} exact={true} />
+      <Route path="/:id" component={PostPage} />
+    </>
   );
 }
 
