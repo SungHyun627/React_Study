b33f5a5129383cfc6b6d9b38a6e3995cd7e25e72
SungHyun627 <kksscc4@gmail.com>
2021-11-28 16:29:22 +0900

    다른 포스트를 읽을 때 이전 리스트가 보여지는 문제 해결

--- redux-middleware-in-react/src/lib/asyncUtils.js
+++ redux-middleware-in-react/src/lib/asyncUtils.js
@@ -46,14 +46,14 @@ export const reducerUtils = {
 
 // 비동기 관련 액션들을 처리하는 리듀서
 // type: 액션 타입, key : 상태의 key(posts, post)
-export const handleAsyncActions = (type, key) => {
+export const handleAsyncActions = (type, key, keepData = false) => {
   const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
   return (state, action) => {
     switch (action.type) {
       case type:
         return {
           ...state,
-          [key]: reducerUtils.loading(),
+          [key]: reducerUtils.loading(keepData ? state[key].data : null),
         };
       case SUCCESS:
         return {
@@ -63,7 +63,7 @@ export const handleAsyncActions = (type, key) => {
       case ERROR:
         return {
           ...state,
-          [key]: reducerUtils.error(action.payload),
+          [key]: reducerUtils.error(action.error),
         };
       default:
         return state;
--- redux-middleware-in-react/src/modules/posts.js
+++ redux-middleware-in-react/src/modules/posts.js
@@ -22,9 +22,14 @@ const GET_POST_SUCCESS = "GET_POST_SUCCESS";
 // 요청 실패
 const GET_POST_ERROR = "GET_POST_ERROR";
 
+// 포스트 비우기
+const CLEAR_POST = "CLEAR_POST";
+
 // thunk 함수 리팩토링
 export const getPosts = createPromiseThunk(GET_POSTS, postAPI.getPosts);
 export const getPost = createPromiseThunk(GET_POST, postAPI.getPostById);
+
+export const clearPost = () => ({ type: CLEAR_POST });
 
 // initial()함수를 이용해서 리팩토링
 const initialState = {
@@ -37,11 +42,16 @@ export default function posts(state = initialState, action) {
     case GET_POSTS:
     case GET_POSTS_SUCCESS:
     case GET_POSTS_ERROR:
-      return handleAsyncActions(GET_POSTS, 'posts')(state, action)
+      return handleAsyncActions(GET_POSTS, "posts", true)(state, action);
     case GET_POST:
     case GET_POST_SUCCESS:
     case GET_POST_ERROR:
-      return handleAsyncActions(GET_POST, 'post')(state, action)
+      return handleAsyncActions(GET_POST, "post")(state, action);
+    case CLEAR_POST:
+      return {
+        ...state,
+        post: reducerUtils.initial(),
+      };
     default:
       return state;
   }
