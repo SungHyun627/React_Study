ffe69f9f2556bcf7cba29d03d6c2733c77113c81
SungHyun627 <kksscc4@gmail.com>
2021-11-28 15:43:54 +0900

    포스트 목록을 보여줄 presentational component 작성

--- redux-middleware-in-react/src/components/PostList.js
+++ redux-middleware-in-react/src/components/PostList.js
@@ -1,9 +1,9 @@
 function PostList({ posts }) {
   return (
     <ul>
-      {posts.map((post) => {
-        <li key={post.id}>{post.title}</li>;
-      })}
+      {posts.map((post) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
     </ul>
   );
 }
