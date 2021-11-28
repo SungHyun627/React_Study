486407690278ee7d8b40f2489b7f50511eee479b
SungHyun627 <kksscc4@gmail.com>
2021-11-28 13:35:54 +0900

    create chunk function

--- redux-middleware-in-react/src/modules/counter.js
+++ redux-middleware-in-react/src/modules/counter.js
@@ -6,6 +6,15 @@ const DECREASE = "DECREASE";
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
 
+// thunk 함수 생성
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(increase()), 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(decrease()), 1000);
+};
+
 // 초기값
 const initialState = 0;
 
