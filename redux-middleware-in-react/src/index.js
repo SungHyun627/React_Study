daa00b906bcd3a02cdb369252c146fa8dd8fe8e3
SungHyun627 <kksscc4@gmail.com>
2021-11-29 13:13:08 +0900

    redux-saga 미들웨어 적용

--- redux-middleware-in-react/src/index.js
+++ redux-middleware-in-react/src/index.js
@@ -5,25 +5,33 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 // import myLogger from "./middlewares/myLogger";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
 
 const customHistory = createBrowserHistory();
+// 사가 미들웨어 생성
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
     applyMiddleware(
       ReduxThunk.withExtraArgument({ history: customHistory }),
+      // 사가 미들웨어 적용
+      sagaMiddleware,
       logger
     )
   )
 );
+
+// 루트 사가 실행
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
