ff6eb6ca50315fc95d69baa98c97389e4253624e
SungHyun627 <kksscc4@gmail.com>
2021-11-29 13:44:55 +0900

    루트 사가에 등록

--- redux-middleware-in-react/src/modules/index.js
+++ redux-middleware-in-react/src/modules/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import counter, { counterSaga } from "./counter";
-import posts from "./posts";
+import posts, { postsSaga } from "./posts";
 import { all } from "redux-saga/effects";
 const rootReducer = combineReducers({ counter, posts });
 export function* rootSaga() {
   // all은 배열 안의 여러 사가를 동시에 실행
-  yield all([counterSaga()]);
+  yield all([counterSaga(), postsSaga()]);
 }
 export default rootReducer;
