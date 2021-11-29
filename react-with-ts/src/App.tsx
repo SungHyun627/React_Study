30b9f3ef8647c926c4895a83ed39fddbaf89ca7b
SungHyun627 <kksscc4@gmail.com>
2021-11-29 20:48:57 +0900

    작성한 component App에 렌더링

--- react-with-ts/src/App.tsx
+++ react-with-ts/src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Greetings from "./Greetings";
+import Counter from "./Counter";
+import MyForm from "./MyForm";
+import ReducerSample from "./ReducerSample";
 
 const App: React.FC = () => {
   const onClick = (name: string) => {
     console.log(`${name} says hello`);
   };
-  return <Greetings name="React" onClick={onClick} />;
+
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return (
+    <>
+      <Greetings name="React" onClick={onClick} />;
+      <Counter />
+      <MyForm onSubmit={onSubmit} />;
+      <ReducerSample />;
+    </>
+  );
 };
 
 export default App;
