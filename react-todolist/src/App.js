55904246632c21ba092cacda81c47f7f4695c5ea
Sunghyun627 <kksscc4@gmail.com>
2022-05-10 14:29:55 +0900

    chore: fix typo

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -1,9 +1,9 @@
-import { createGlobalStyle } from "styled-components";
-import TodoTemplate from "./components/TodoTemplate";
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
-import TodoCreate from "./components/TodoCreate";
-import { TodoProvider } from "./components/TodoContext";
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+import { TodoProvider } from './components/TodoContext';
 
 const GlobalStyle = createGlobalStyle`
   body {
