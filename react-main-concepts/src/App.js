d2a75b9e7eb09b1766806654defa1eaa5efa1de5
Sunghyun627 <kksscc4@gmail.com>
2022-05-11 19:20:53 +0900

    12. Thinking in React : build searchable product data table

--- react-main-concepts/src/App.js
+++ react-main-concepts/src/App.js
@@ -10,8 +10,42 @@ import Reservation from './components/Reservation';
 import Calculator from './components/temperature_calculator/Calculator';
 import WelcomeDialog from './components/Dialog/WelcomeDialog';
 import SignUpDialog from './components/Dialog/SignUpDialog';
+import FilterableProductTable from './components/ProductFilter/FilterableProductTable';
 
 const numbers = [1, 2, 3, 4, 5];
+const products = [
+  {
+    category: 'Sporting Goods',
+    price: '$49.99',
+    stocked: true,
+    name: 'Football',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$9.99',
+    stocked: true,
+    name: 'Baseball',
+  },
+  {
+    category: 'Sporting Goods',
+    price: '$29.99',
+    stocked: false,
+    name: 'Basketball',
+  },
+  {
+    category: 'Electronics',
+    price: '$99.99',
+    stocked: true,
+    name: 'iPod Touch',
+  },
+  {
+    category: 'Electronics',
+    price: '$399.99',
+    stocked: false,
+    name: 'iPhone 5',
+  },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' },
+];
 
 function App() {
   return (
@@ -28,6 +62,7 @@ function App() {
       <Calculator />
       <WelcomeDialog />
       <SignUpDialog />
+      <FilterableProductTable products={products} />
     </div>
   );
 }
